2036cdf3b62ac8d35edf2e04ad7ce9f7835d6319
TEAMOS-PC\Siam <f>
2026-03-18 00:01:07 +0600

    seeder

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Student;
+use Illuminate\Support\Facades\File;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //to insert single data into students table using model factory
+        // Student::create([
+        //     'name' => 'Siam',
+        //     'age' => 25,
+        //     'email' => 'siam@example.com',
+        //     'address' => 'Dhaka',
+        //     'gender' => 'male',
+        //     'phone' => 1234567890
+        // ]);
+
+
+        //to insert multiple data into students table using model factory
+    //    $student = collect([
+    //      [
+    //         'name' => 'Siam',
+    //         'age' => 25,
+    //         'email' => 'siam@example.com',
+    //         'address' => 'Dhaka',
+    //         'gender' => 'male',
+    //         'phone' => 1234567890
+    //         ],
+    //         [
+    //         'name' => 'Shamim',
+    //         'age' => 30,
+    //         'email' => 'shamim@example.com',
+    //         'address' => 'Dhaka',
+    //         'gender' => 'male',
+    //         'phone' => 123456774
+    //         ],
+            
+    //         [
+    //         'name' => 'Sadia',
+    //         'age' => 22,
+    //         'email' => 'sadia@example.com',
+    //         'address' => 'Dhaka',
+    //         'gender' => 'female',
+    //         'phone' => 1234567890
+    //         ]
+    //    ]);
+    //      $student->each(function($student){
+    //       Student::create($student);
+    //      });
+
+
+    //to insert multiple data into students using json file data
+        $json = File::get("database/json/students.json");
+        $students = collect(json_decode($json));
+        $students->each(function($student){
+            Student::create([
+                'name' => $student->name,
+                'age' => $student->age,
+                'email' => $student->email,
+                'address' => $student->address,
+                'gender' => $student->gender,
+                'phone' => $student->phone
+            ]);
+        });
+        
+    }
+}
